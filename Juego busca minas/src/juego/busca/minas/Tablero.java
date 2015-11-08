f206d3854337d09458b5d6b4122ec77c6f4b5ee0
Maikol1986 <Xaviermr19@hotmail.com>
2015-11-08 14:22:00 -0600

    :tada: Added .gitattributes & .gitignore files

---
+++ Juego busca minas/src/juego/busca/minas/Tablero.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego.busca.minas;
+
+/**
+ *
+ * @author Maikol
+ */
+public class Tablero {
+  private String tablero[][];
+  private int minas[][];
+  private boolean elegidos[][];
+  private int filas;
+  private int columnas;
+}
+
+  public class Tablero(){
+  filas=6;
+  columnas=6;
+  tablero=new String[filas][columnas];
+
+}
+  
+  
+
